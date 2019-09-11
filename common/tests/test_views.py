d915b4b52015341d84a338435490f1fdbc607a61
Felipe Lee <felipe.lee.garcia@gmail.com>
2019-09-11 13:40:11 -0500

    Add simple test for home page

---
+++ common/tests/test_views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for common views
+"""
+from django.test import TestCase
+from django.urls import reverse_lazy
+
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self) -> None:
+        response = self.client.get(reverse_lazy('home'))
+
+        self.assertTemplateUsed(response, 'common/home.html')
