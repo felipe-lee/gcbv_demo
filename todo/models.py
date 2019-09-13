a30554f22ee64c9f094ca215dddc48ecdb8ab9fc
Felipe Lee <felipe.lee.garcia@gmail.com>
2019-09-12 23:00:39 -0500

    Add simple TodoListModel and TodoItemModel
    Likely not what I'll use in the end but good enough for now

--- todo/models.py
+++ todo/models.py
@@ -2,5 +2,27 @@
 """
 models for todo app
 """
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class TodoListModel(models.Model):
+    """
+    Model to keep todo lists
+    """
+    name = models.CharField(verbose_name=_('list name'), max_length=200)
+
+
+class TodoItemModel(models.Model):
+    """
+    Model for todo items
+    """
+    todo_list = models.ForeignKey(TodoListModel, default=None, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('todo_list', 'text')
+
+    def __str__(self) -> str:
+        return self.text
