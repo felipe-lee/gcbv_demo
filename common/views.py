f3b46ebc07962d96df1f5a4382106661ce31731e
Felipe Lee <felipe.lee.garcia@gmail.com>
2019-09-17 23:05:40 -0500

    Add fbv for base home page and set up base urls to handle cbv vs fbv

--- common/views.py
+++ common/views.py
@@ -2,6 +2,8 @@
 """
 Common Views
 """
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
@@ -10,3 +12,12 @@ class HomeView(TemplateView):
     Render home page
     """
     template_name = 'common/home.html'
+
+
+def home_view(request: HttpRequest) -> HttpResponse:
+    """
+    Render todo home page
+    :param request: user wsgi request
+    :return: home page
+    """
+    return render(request=request, template_name='common/home.html')
--- gcbv_demo/urls.py
+++ gcbv_demo/urls.py
@@ -2,13 +2,18 @@
 """
 gcbv_demo URL Configuration
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-from common.views import HomeView
+from common.views import HomeView, home_view
 
 urlpatterns = [
     path('todo/', include('todo.urls', namespace='todo')),
     path('mage/', admin.site.urls),
-    path('', HomeView.as_view(), name='home')
 ]
+
+if settings.VIEW_TYPES == 'CBV':
+    urlpatterns.append(path('', HomeView.as_view(), name='home'))
+else:
+    urlpatterns.append(path('', home_view, name='home'))
